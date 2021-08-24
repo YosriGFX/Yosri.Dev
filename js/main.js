54afd34159bea45e4da985f7c2e87f584eb9adfe
YosriGFX <YosriG1997@gmail.com>
2021-08-24 20:54:25 +0100

    Priv design

--- js/main.js
+++ js/main.js
@@ -13,3 +13,20 @@ function show_hide() {
         }, 2000);
     }
 }
+
+
+function contact_pop() {
+    var menu = document.getElementById('contact_pop');
+    if (menu.classList.contains("hide")){
+        menu.classList.remove("hide");
+        setTimeout(function(){
+          menu.style.display = 'block';
+        }, 1);
+    }
+    else{
+        menu.classList.add("hide");
+        setTimeout(function(){
+          menu.style.display = 'none';
+        }, 1000);
+    }
+}
